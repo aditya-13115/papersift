db46bd028f0ac96bdb5701a67b15b8301b0d744f
Aditya Das <142023190+aditya-13115@users.noreply.github.com>
2026-08-13 04:05:30 +0000

    feat: add non-retryable LLM error handling

--- agenticResearch/app/main.py
+++ agenticResearch/app/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from groq import AsyncGroq
+import groq
 import asyncio
 
 
@@ -11,38 +12,54 @@ if not api_key:
     raise ValueError("GROQ_API_KEY is not set")
 
 
+NON_RETRYABLE_ERRORS = (
+    groq.BadRequestError,          # 400
+    groq.AuthenticationError,      # 401
+    groq.PermissionDeniedError,    # 403
+    groq.NotFoundError,            # 404
+    groq.UnprocessableEntityError, # 422
+)
+
 
 class AsyncLLMClient():
     def __init__(self, model: str, temperature: float = 0.95, max_completion_tokens: int = 1024):
         self.model = model
         self.temperature = temperature
         self.max_completion_tokens = max_completion_tokens
+        self.client = AsyncGroq(api_key=api_key)
 
     async def generate(self, prompt: str):
-        client = AsyncGroq(api_key=api_key)
         
-        chat_completion = await client.chat.completions.create( 
-                messages=[
-                    
-                    {
-                        "role": "system",
-                        "content": "You are a helpful assistant."
-                    },
-                    # Set a user message for the assistant to respond to.
-                    {
-                        "role": "user",
-                        "content": prompt,
-                    }
-                ],
-                model=self.model, temperature=self.temperature, max_completion_tokens=self.max_completion_tokens)
-        
-        return chat_completion.choices[0].message.content
+        try:
+            chat_completion = await self.client.chat.completions.create( 
+                    messages=[
+                        
+                        {
+                            "role": "system",
+                            "content": "You are a helpful assistant."
+                        },
+                        # Set a user message for the assistant to respond to.
+                        {
+                            "role": "user",
+                            "content": prompt,
+                        }
+                    ],
+                    model=self.model, temperature=self.temperature, max_completion_tokens=self.max_completion_tokens)
+            
+            return chat_completion.choices[0].message.content
+
+        except NON_RETRYABLE_ERRORS as e:
+            print(type(e))
+            print(e)
+            print("Non-retryable error.")
+            raise
 
 
 async def main():
     client = AsyncLLMClient(model="groq/compound")
+
     result = await client.generate("Explain asyncio in simple terms")
     print(result)
 
-asyncio.run(main())
 
+asyncio.run(main())
